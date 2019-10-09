1e0efbc8cbf6e3b2dd1fe54b28b7cfc94f96b210
DmK78 <dmk78@inbox.ru>
2019-10-09 17:06:54 +0500

    сделал синглтон

--- app/src/main/java/ru/job4j/daggerexample/StoreComponent.java
+++ app/src/main/java/ru/job4j/daggerexample/StoreComponent.java
@@ -1,7 +1,9 @@
 package ru.job4j.daggerexample;
 
-import dagger.Component;
+import javax.inject.Singleton;
 
+import dagger.Component;
+@Singleton
 @Component(modules = StoreModule.class)
 public interface StoreComponent {
     void injectTo(MainActivity activity);
--- app/src/main/java/ru/job4j/daggerexample/StoreModule.java
+++ app/src/main/java/ru/job4j/daggerexample/StoreModule.java
@@ -1,10 +1,13 @@
 package ru.job4j.daggerexample;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class StoreModule {
+    @Singleton
     @Provides
     public Store providesStore() {
         return new MemStore();
